9b7b6b0232ff50f36c6ce38c50e8274db94340bc
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-05-21 22:00:20 -0700

    modified Lombok.java

--- src/com/projects/lombok/data/LombokDataDemo.java
+++ src/com/projects/lombok/data/LombokDataDemo.java
@@ -1,4 +1,34 @@
 package com.projects.lombok.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
 public class LombokDataDemo {
+
+    private  String name;
+
+    private int age;
+
+    private double salary;
+
+    public static void main(String[] args) {
+
+        LombokDataDemo obj1 = new LombokDataDemo();
+
+        obj1.setName("Kuldeep");
+
+        obj1.setSalary(9000);
+
+        obj1.setAge(30);
+
+        System.out.println(obj1.getName());
+
+        System.out.println(obj1.hashCode());
+
+    }
+
+
+
 }
